b02d3d4589708591198b8ec7b652622e7de59cfb
mamamot <mamamot@gmail.com>
2016-06-10 20:44:23 +0300

    Nabokov: loading paragraphs into lists for comparison.

--- Nabokov/file_comparison.py
+++ Nabokov/file_comparison.py
@@ -3,9 +3,7 @@ from lxml import etree
 
 
 def merge_trees(path):
-    root = etree.Element("html")
-    head = etree.SubElement(root, "head")
-    body = etree.SubElement(root, "body")
+    paras = list()
     para_counter = 0
     for file in os.listdir(path):
         try:
@@ -13,13 +11,26 @@ def merge_trees(path):
                 file_tree = etree.fromstring(f.read())
                 for input_para in file_tree.iter('para'):
                     input_para.set('id', str(para_counter))
-                    body.append(input_para)
+                    paras.append(input_para)
                     para_counter += 1
         except Exception as e:
             print(e)
-    return root
+    return paras
+
+
+def load_model(path):
+    paras = list()
+    try:
+        with open(path, encoding="utf-8") as f:
+            tree = etree.fromstring(f.read())
+            paras = list(tree.iter('para'))
+    except Exception as e:
+        print(e)
+    return paras
 
 
 merged = merge_trees("Pnin/Pnin_iljin/")
-with open("merged.xml", "wb") as w:
-    w.write(etree.tostring(merged, method="html", pretty_print=True, encoding="utf-8"))
+model = load_model("Pnin/pnin_barabtarlo.xml")
+print(len(merged))
+print(len(model))
+
