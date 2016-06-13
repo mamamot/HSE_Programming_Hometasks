69666deb117deeb2e466f1e7fee3b0dca7d12ee9
mamamot <mamamot@gmail.com>
2016-06-14 01:21:25 +0300

    Nabokov: tested and fixed. Sample output of 'Pnin' added. Console log added as well, boasting the 255 fixed errors in Iljin's translation.

--- Nabokov/file_comparison.py
+++ Nabokov/file_comparison.py
@@ -36,6 +36,7 @@ def load_model(path):
 
 def compare_and_fix(model, bad):
     counter = 1
+    print("Correction phase 1 commencing.")
     for x in range(1, len(model)-1):
         print(x, end=" ", flush=True)
         for y in range(1, len(bad)-1):
@@ -52,19 +53,58 @@ def compare_and_fix(model, bad):
                     bad[y].set("id", old_index)
                     counter += 1
                     break
+    print()
+    print("Correction phase 2 commencing.")
+    for x in range(1, len(model) - 2):
+        print(x, end=" ", flush=True)
+        for y in range(1, len(bad) - 2):
+            if model[x - 1].xpath(".//se[@lang='en']/text()") == bad[y - 1].xpath(".//se[@lang='en']/text()") and \
+                            model[x + 2].xpath(".//se[@lang='en']/text()") == bad[y + 2].xpath(
+                        ".//se[@lang='en']/text()"):
+                if model[x].xpath(".//se[@lang='en']/text()") != bad[y].xpath(".//se[@lang='en']/text()"):
+                    print()
+                    print("{}. Found error".format(counter))
+                    print(model[x].xpath(".//se[@lang='en']/text()")[0])
+                    print(bad[y].xpath(".//se[@lang='en']/text()")[0])
+                    print()
+                    old_index = bad[y].get("id")
+                    bad[y] = model[x]
+                    bad[y].set("id", old_index)
+                    counter += 1
+                    break
+    print()
+    print("Correction phase 3 commencing.")
+    for x in range(1, len(model) - 3):
+        print(x, end=" ", flush=True)
+        for y in range(1, len(bad) - 3):
+            if model[x - 1].xpath(".//se[@lang='en']/text()") == bad[y - 1].xpath(".//se[@lang='en']/text()") and \
+                            model[x + 3].xpath(".//se[@lang='en']/text()") == bad[y + 3].xpath(
+                        ".//se[@lang='en']/text()"):
+                if model[x].xpath(".//se[@lang='en']/text()") != bad[y].xpath(".//se[@lang='en']/text()"):
+                    print()
+                    print("{}. Found error".format(counter))
+                    print(model[x].xpath(".//se[@lang='en']/text()")[0])
+                    print(bad[y].xpath(".//se[@lang='en']/text()")[0])
+                    print()
+                    old_index = bad[y].get("id")
+                    bad[y] = model[x]
+                    bad[y].set("id", old_index)
+                    counter += 1
+                    break
     return build_output_tree(bad)
 
 
 def build_output_tree(para_list):
+    print("Comparison done. Building output of lenght ".format(len(para_list)))
     root = etree.Element("html")
     head = etree.SubElement(root, "head")
     body = etree.SubElement(root, "body")
     for para in para_list:
         body.append(para)
-    return root
+    return etree.tostring(root, method="html", encoding="utf-8", pretty_print=True)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     if len(sys.argv) == 4:
         model_path = sys.argv[1]
         bad_path = sys.argv[2]
@@ -72,8 +112,8 @@ if __name__=="__main__":
         model = load_model(model_path)
         fixed = compare_and_fix(model, merged)
         try:
-            with open(sys.argv[3], "w", encoding="utf-8") as w:
-                w.write(etree.tostring(fixed, method="html", encoding="utf-8", pretty_print=True))
+            with open(sys.argv[3], "wb") as w:
+                w.write(fixed)
         except Exception as e:
             print("Error writing output: " + str(e))
     else:
